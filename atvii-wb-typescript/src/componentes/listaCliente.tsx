1a90215ac24d522f38c5b54ea135da8e16046623
thaleskerber <thaleskerber@gmail.com>
2022-11-07 13:08:54 -0300

    lista 2

--- atvii-wb-typescript/src/componentes/listaCliente.tsx
+++ atvii-wb-typescript/src/componentes/listaCliente.tsx
@@ -1,7 +1,9 @@
+/* eslint-disable @typescript-eslint/no-useless-constructor */
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import { Component } from "react";
 import 'materialize-css/dist/css/materialize.min.css'
 import M from 'materialize-css'
+import './myStyle.css'
 
 
 
@@ -20,55 +22,198 @@ export default class ListaCliente extends Component<props> {
 
     render() {
         return (
-            <div>
-                <ul className="collapsible">
-                    <li >
-                        <div className="collapsible-header"><i className="large material-icons">account_circle</i>Cliente 1<i className="large material-icons">create</i><i className="large material-icons">delete</i></div>
-                        <div className="collapsible-body">
-                            <span>Nome Social:</span><br />
-                            <span>Sexo:</span><br />
-                            <span>RG:</span><br />
-                            <span>Data Emissão:</span><br />
-                            <span>CPF:</span><br />
-                            <span>Data Emissão:</span><br />
-                            <span>DDD:</span><br />
-                            <span>Telefone:</span><br />
-                        </div>
-                    </li>
-                    <li className="active">
-                        <div className="collapsible-header"><i className="large material-icons">account_circle</i>Cliente 2<i className="large material-icons">create</i><i className="large material-icons">delete</i></div>
-                        <div className="collapsible-body">
-                            <span>Nome Social:</span><br />
-                            <span>Sexo:</span><br />
-                            <span>RG:</span><br />
-                            <span>Data Emissão:</span><br />
-                            <span>CPF:</span><br />
-                            <span>Data Emissão:</span><br />
-                            <span>DDD:</span><br />
-                            <span>Telefone:</span><br />
-                        </div>
-                    </li>
-                    <li >
-                        <div className="row no-padding collapsible-header">
-                            <i className="col s12 l1 blue large material-icons">account_circle</i>
-                            <p className="col s12 l7 blue">Cliente 3</p>
-                            <i className="col s12 l2 blue large material-icons">create</i>
-                            <i className="col s12 l2 blue large material-icons">delete</i></div>
-                        <div className="collapsible-body">
-                            <span>Nome Social:</span><br />
-                            <span>Sexo:</span><br />
-                            <span>RG:</span><br />
-                            <span>Data Emissão:</span><br />
-                            <span>CPF:</span><br />
-                            <span>Data Emissão:</span><br />
-                            <span>DDD:</span><br />
-                            <span>Telefone:</span><br />
-                        </div>
-                    </li>
-                </ul>
-                <div className="">
-                    <div className="right">
-                        <i className="medium material-icons">add_circle</i>
+            <div id="backgroundClientContainer">
+                <div id="titleContainer">
+                    <h4 id="clientTitle">Clientes cadastrados</h4>
+                </div>
+                <div id="collapsibleContainer">
+                    <ul className="collapsible">
+                        <li className="active" >
+                            <div id="collapsibleHeader" className="collapsible-header">
+                                Vitoria
+                            </div>
+                            <div id="collapsibleBody" className="collapsible-body">
+                                <span>Nome Social: Vizz</span><br />
+                                <span>Sexo: F</span><br />
+                                <span>RG: 123456789</span><br />
+                                <span>Data Emissão: 12/12/2020</span><br />
+                                <span>CPF: 12345678912</span><br />
+                                <span>Data Emissão: 12/12/2021</span><br />
+                                <span>DDD: 12</span><br />
+                                <span>Telefone: 12345678</span><br />
+
+                                <div id="editDeleteButtonContainer">
+                                    <a href="#" id="editDeleteButton" className="btn-floating btn-medium pulse"><i className="small material-icons">create</i></a>
+                                    <a href="#" id="editDeleteButton" className="btn-floating btn-medium pulse"><i className="small material-icons">delete</i></a>
+                                </div>
+                            </div>
+                        </li>
+                        <li className="">
+                            <div id="collapsibleHeader" className="collapsible-header">
+                                Thales
+                            </div>
+                            <div id="collapsibleBody" className="collapsible-body">
+                                <span>Nome Social: TK</span><br />
+                                <span>Sexo: M</span><br />
+                                <span>RG: 987654321</span><br />
+                                <span>Data Emissão: 12/12/2021</span><br />
+                                <span>CPF: 11987654321</span><br />
+                                <span>Data Emissão: 12/12/2021</span><br />
+                                <span>DDD: 12</span><br />
+                                <span>Telefone: 432749471</span><br />
+
+                                <div id="editDeleteButtonContainer">
+                                    <a href="#" id="editDeleteButton" className="btn-floating btn-medium pulse"><i className="small material-icons">create</i></a>
+                                    <a href="#" id="editDeleteButton" className="btn-floating btn-medium pulse"><i className="small material-icons">delete</i></a>
+                                </div>
+                            </div>
+                        </li>
+                        <li >
+                            <div id="collapsibleHeader" className="collapsible-header">
+                                Francisco
+                            </div>
+                            <div id="collapsibleBody" className="collapsible-body">
+                                <span>Nome Social: Fran</span><br />
+                                <span>Sexo: O</span><br />
+                                <span>RG: 987372180</span><br />
+                                <span>Data Emissão: 17/09/2005</span><br />
+                                <span>CPF: 09612739401</span><br />
+                                <span>Data Emissão: 17/09/2005</span><br />
+                                <span>DDD: 12</span><br />
+                                <span>Telefone: 983618492</span><br />
+
+                                <div id="editDeleteButtonContainer">
+                                    <a href="#" id="editDeleteButton" className="btn-floating btn-medium pulse"><i className="small material-icons">create</i></a>
+                                    <a href="#" id="editDeleteButton" className="btn-floating btn-medium pulse"><i className="small material-icons">delete</i></a>
+                                </div>
+                            </div>
+                        </li>
+                        <li>
+                            <div id="collapsibleHeader" className="collapsible-header">
+                                Fabiana
+                            </div>
+                            <div id="collapsibleBody" className="collapsible-body">
+                                <span>Nome Social: Vizz</span><br />
+                                <span>Sexo: F</span><br />
+                                <span>RG: 123456789</span><br />
+                                <span>Data Emissão: 12/12/2020</span><br />
+                                <span>CPF: 12345678912</span><br />
+                                <span>Data Emissão: 12/12/2021</span><br />
+                                <span>DDD: 12</span><br />
+                                <span>Telefone: 12345678</span><br />
+
+                                <div id="editDeleteButtonContainer">
+                                    <a href="#" id="editDeleteButton" className="btn-floating btn-medium pulse"><i className="small material-icons">create</i></a>
+                                    <a href="#" id="editDeleteButton" className="btn-floating btn-medium pulse"><i className="small material-icons">delete</i></a>
+                                </div>
+                            </div>
+                        </li>
+                        <li className="">
+                            <div id="collapsibleHeader" className="collapsible-header">
+                                Adriana
+                            </div>
+                            <div id="collapsibleBody" className="collapsible-body">
+                                <span>Nome Social: TK</span><br />
+                                <span>Sexo: M</span><br />
+                                <span>RG: 987654321</span><br />
+                                <span>Data Emissão: 12/12/2021</span><br />
+                                <span>CPF: 11987654321</span><br />
+                                <span>Data Emissão: 12/12/2021</span><br />
+                                <span>DDD: 12</span><br />
+                                <span>Telefone: 432749471</span><br />
+
+                                <div id="editDeleteButtonContainer">
+                                    <a href="#" id="editDeleteButton" className="btn-floating btn-medium pulse"><i className="small material-icons">create</i></a>
+                                    <a href="#" id="editDeleteButton" className="btn-floating btn-medium pulse"><i className="small material-icons">delete</i></a>
+                                </div>
+                            </div>
+                        </li>
+                        <li >
+                            <div id="collapsibleHeader" className="collapsible-header">
+                                Breno
+                            </div>
+                            <div id="collapsibleBody" className="collapsible-body">
+                                <span>Nome Social: Fran</span><br />
+                                <span>Sexo: O</span><br />
+                                <span>RG: 987372180</span><br />
+                                <span>Data Emissão: 17/09/2005</span><br />
+                                <span>CPF: 09612739401</span><br />
+                                <span>Data Emissão: 17/09/2005</span><br />
+                                <span>DDD: 12</span><br />
+                                <span>Telefone: 983618492</span><br />
+
+                                <div id="editDeleteButtonContainer">
+                                    <a href="#" id="editDeleteButton" className="btn-floating btn-medium pulse"><i className="small material-icons">create</i></a>
+                                    <a href="#" id="editDeleteButton" className="btn-floating btn-medium pulse"><i className="small material-icons">delete</i></a>
+                                </div>
+                            </div>
+                        </li>
+                        <li>
+                            <div id="collapsibleHeader" className="collapsible-header">
+                                Maria
+                            </div>
+                            <div id="collapsibleBody" className="collapsible-body">
+                                <span>Nome Social: Vizz</span><br />
+                                <span>Sexo: F</span><br />
+                                <span>RG: 123456789</span><br />
+                                <span>Data Emissão: 12/12/2020</span><br />
+                                <span>CPF: 12345678912</span><br />
+                                <span>Data Emissão: 12/12/2021</span><br />
+                                <span>DDD: 12</span><br />
+                                <span>Telefone: 12345678</span><br />
+
+                                <div id="editDeleteButtonContainer">
+                                    <a href="#" id="editDeleteButton" className="btn-floating btn-medium pulse"><i className="small material-icons">create</i></a>
+                                    <a href="#" id="editDeleteButton" className="btn-floating btn-medium pulse"><i className="small material-icons">delete</i></a>
+                                </div>
+                            </div>
+                        </li>
+                        <li className="">
+                            <div id="collapsibleHeader" className="collapsible-header">
+                                Juliana
+                            </div>
+                            <div id="collapsibleBody" className="collapsible-body">
+                                <span>Nome Social: TK</span><br />
+                                <span>Sexo: M</span><br />
+                                <span>RG: 987654321</span><br />
+                                <span>Data Emissão: 12/12/2021</span><br />
+                                <span>CPF: 11987654321</span><br />
+                                <span>Data Emissão: 12/12/2021</span><br />
+                                <span>DDD: 12</span><br />
+                                <span>Telefone: 432749471</span><br />
+
+                                <div id="editDeleteButtonContainer">
+                                    <a href="#" id="editDeleteButton" className="btn-floating btn-medium pulse"><i className="small material-icons">create</i></a>
+                                    <a href="#" id="editDeleteButton" className="btn-floating btn-medium pulse"><i className="small material-icons">delete</i></a>
+                                </div>
+                            </div>
+                        </li>
+                        <li >
+                            <div id="collapsibleHeader" className="collapsible-header">
+                                César
+                            </div>
+                            <div id="collapsibleBody" className="collapsible-body">
+                                <span>Nome Social: Fran</span><br />
+                                <span>Sexo: O</span><br />
+                                <span>RG: 987372180</span><br />
+                                <span>Data Emissão: 17/09/2005</span><br />
+                                <span>CPF: 09612739401</span><br />
+                                <span>Data Emissão: 17/09/2005</span><br />
+                                <span>DDD: 12</span><br />
+                                <span>Telefone: 983618492</span><br />
+
+                                <div id="editDeleteButtonContainer">
+                                    <a href="#" id="editDeleteButton" className="btn-floating btn-medium pulse"><i className="small material-icons">create</i></a>
+                                    <a href="#" id="editDeleteButton" className="btn-floating btn-medium pulse"><i className="small material-icons">delete</i></a>
+                                </div>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+                <div id="addButtonContainer">
+                    <div id="teste">
+                        <a href="#" className="btn-floating btn-large pulse"><i className="large material-icons">add</i></a>
+
                     </div>
                 </div>
             </div>
